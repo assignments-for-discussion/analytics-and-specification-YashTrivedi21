00460c6dbf108ed065672952ff59c46d66872265
Yash Trivedi <ybt2027@gmail.com>
2021-12-07 17:37:28 +0530

    Update average.js

--- average.js
+++ average.js
@@ -1,8 +1,6 @@
-
 // eslint-disable-next-line complexity
 function average(array) {
-  // eslint-disable-next-line max-len
-  // if NaN exist in the given array remove them, now array contains only numbers
+  // if NaN exist in the given array remove them, now array contains only number
   let numbers = array.filter( (value) => !Number.isNaN(value) );
   // if array is empty return NaN;
   if (!numbers.length) return NaN;
@@ -30,7 +28,7 @@ function average(array) {
   // setting a condition such that if a number is beyond
   // mean +/- standard deviation then there is a fault in the sensor
   // eslint-disable-next-line max-len
-  // i have followed below link which says the accurate values of any data are within +/- 2*standard deviation.
+  // i have followed below link which says the accurate values of any data are within +/- 2 * standard deviation.
   // https://www.labce.com/spg49741_acceptable_standard_deviation_sd.aspx#:~:text=Statisticians%20have%20determined%20that%20values,area%20greater%20than%20%C2%B1%202SD.
   // eslint-disable-next-line max-len
   const check = (element) => (element > (mean + 2*dev) || element < (mean - 2*dev));
